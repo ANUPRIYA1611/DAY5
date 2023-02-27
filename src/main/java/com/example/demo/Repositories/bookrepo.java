ff81659c4ddaeeefcaea9e5948858305faadfc23
anupriya <727721euai004@skcet.ac.in>
2023-02-27 15:07:46 +0530

    day5

---
+++ src/main/java/com/example/demo/Repositories/bookrepo.java
@@ -0,0 +1,7 @@
+package com.example.demo.Repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Models.book;
+public interface bookrepo  extends JpaRepository<book,Integer>{
+
+}
